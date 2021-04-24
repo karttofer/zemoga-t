96635f732fed8eb2dd8c5d398fc9abaa17a9b141
Jhornan Colina <jhornancolina@gmail.com>
2021-04-23 19:42:35 -0500

    add imgs and mock data

--- src/store/initialState.js
+++ src/store/initialState.js
@@ -4,9 +4,12 @@ import { createStore } from 'redux';
 // Reducers
 import { addElements } from './reducers';
 
+// Mock Data
+import { mock } from '../commons/mockData/mock';
+
 // Initial State
 export const initialState = {
-  items: [],
+  items: mock,
 };
 
 // Store
